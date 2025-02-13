718c89fa25c09b5fcd5f01b5cce7876faf0d2268
Myrone Silverio <silveriomyrone@gmail.com>
2025-02-13 23:02:21 +0800

    Fixed homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,15 @@
-import { ServicesHero, ServicesList } from "@/components/services";
 import Image from 'next/image';
+import { Hero, Guide, Services, Offers, Events, Testimonials } from '@/components/home';
 
-export default function ServicesListPage() {
+export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center w-svw text-ginaBlack">
-            <ServicesHero />
-            <ServicesList />
+        <div className="flex flex-col items-center justify-center">
+            <Hero />
+            <Guide />
+            <Services />
+            <Offers />
+            <Events />
+            <Testimonials />
         </div>
     );
 }
